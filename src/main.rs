a7f956d7ee865ce3707c2db4981993e840398729
Heinz N. Gies <heinz@licenser.net>
2017-02-09 22:09:38 +0100

    Nicer CLI options

--- src/main.rs
+++ src/main.rs
@@ -16,18 +16,29 @@ use clap::{App, Arg, ArgMatches, ArgGroup};
 pub fn build_cli() -> App<'static, 'static> {
     App::new("mstore-info")
         .about("mstore information utility")
-        .arg(Arg::with_name("index")
-            .value_name("idx")
+        .arg(Arg::with_name("files")
+            .value_name("files")
             .help("Index File")
+            .multiple(true)
             .required(true)
             .index(1))
-        .arg(Arg::with_name("count").long("count").short("c"))
-        .arg(Arg::with_name("offset").long("offset").short("o"))
-        .arg(Arg::with_name("ppf").long("points-per-file").short("p"))
-        .arg(Arg::with_name("human").long("text").short("t"))
-        .group(ArgGroup::with_name("field")
-            .args(&["count", "offset", "ppf", "human"])
-            .required(true))
+        .arg(Arg::with_name("sum")
+            .help("Show the total sum of metrics.")
+            .long("sum")
+            .short("s"))
+        .arg(Arg::with_name("count")
+            .help("Shows the count of metrics in each file.")
+            .long("count")
+            .short("c"))
+        .arg(Arg::with_name("offset")
+            .help("Shows the offset of each file.")
+            .long("offset")
+            .short("o"))
+        .arg(Arg::with_name("ppf")
+            .help("Show the points per file for each file")
+            .long("points-per-file")
+            .short("p"))
+        .group(ArgGroup::with_name("field").args(&["count", "offset", "ppf", "sum"]))
 
 }
 struct MFileIdx {
@@ -55,15 +66,14 @@ fn read_idx(idx: &Path) -> Result<MFileIdx, std::io::Error> {
 }
 
 
-fn print_index(idx: &MFileIdx, matches: &ArgMatches) {
+fn print_index(name: &str, idx: &MFileIdx, matches: &ArgMatches) {
     if matches.is_present("count") {
-        println!("{}", idx.count)
+        println!("{}", idx.count);
     } else if matches.is_present("offset") {
-        println!("{}", idx.offset)
+        println!("{}", idx.offset);
     } else if matches.is_present("ppf") {
-        println!("{}", idx.ppf)
-    } else if matches.is_present("human") {
-        let name = value_t!(matches, "index", String).unwrap();
+        println!("{}", idx.ppf);
+    } else {
         println!("statistics on: {}", name);
         println!("  offset:          {}", idx.offset);
         println!("  points per file: {}", idx.ppf);
@@ -73,12 +83,36 @@ fn print_index(idx: &MFileIdx, matches: &ArgMatches) {
 
 fn main() {
     let matches = build_cli().get_matches();
-    let base = value_t!(matches, "index", String).unwrap();
-    let idx = Path::new(&base);
-    //let idx = path.with_extension("idx");
-    //let mstore = path.with_extension("mstore");
-    match read_idx(idx) {
-        Ok(idx) => print_index(&idx, &matches),
-        Err(_) => println!("illegal index"),
+    //println!("{:?}", matches);
+    let files = matches.values_of("files").unwrap().collect::<Vec<_>>();
+    let mut sum = 0;
+    let mut count = 0;
+    let use_sum = matches.is_present("sum");
+    let print_human = !(matches.is_present("count") || matches.is_present("offset") ||
+                        matches.is_present("ppf"));
+
+    for base in files {
+        let idx = Path::new(&base);
+        count = count + 1;
+        //let idx = path.with_extension("idx");
+        //let mstore = path.with_extension("mstore");
+        match read_idx(idx) {
+            Ok(idx) => {
+                if !use_sum {
+                    print_index(&base, &idx, &matches);
+                };
+                sum = sum + idx.count;
+            }
+            Err(_) => println!("illegal index: {}", base),
+        }
     }
+    if use_sum {
+        println!("{}", sum);
+    }
+    if print_human && count > 1 {
+        println!("\ntotal:");
+        println!("  file count:      {}", count);
+        println!("  metric count:    {}", sum);
+    }
+
 }
